7f2357f0b1f9011ee500644a4478cebcb55e375e
= <=>
2020-03-05 19:59:03 -0800

    Split components up into parts for final solution

---
+++ Joe_CrimeHouseCheck/crimeHouse.js
@@ -0,0 +1,33 @@
+var crimeHouse = "henry";
+
+function checkCrimeHouse(houseNum){
+    switch(houseNum){
+        case 1:
+        if(crimeHouse === "henry"){
+            alert("The crime has been found!");
+        }else{
+            alert("seems to be nothing here");
+        }
+        break;
+        
+        case 2:
+        if(crimeHouse === "alex"){
+            alert("The crime has been found!");
+        }else{
+            alert("seems to be nothing here");
+        }
+        break;
+
+       
+        case 3:
+        if(crimeHouse === "noah"){
+            alert("The crime has been found!");
+        }else{
+            alert("seems to be nothing here");
+        }
+        break;
+
+
+        
+    }
+}
