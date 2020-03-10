f6a0dcccdce5b763901acb76f65f82d412b3505b
= <=>
2020-03-09 19:24:45 -0700

    Check Repeat added but not complete

--- Joe_PageTransitions/transition.js
+++ Joe_PageTransitions/transition.js
@@ -3,5 +3,5 @@
 function pageSlide(current, next){
     document.querySelector("#" + next).style.display = "flex";
     document.querySelector("#" + current).style.display = "none";
-    console.log("test");
+    console.log(next);
 }
