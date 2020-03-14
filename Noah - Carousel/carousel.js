1831ade80a815220cd0c7650de3e18ad96200dea
Noah Mah <noahmah@Noahs-MacBook-Pro.local>
2020-03-13 22:48:55 -0700

    got rid of add/ subtract function idk y no work

--- Noah - Carousel/carousel.js
+++ Noah - Carousel/carousel.js
@@ -1,31 +1,28 @@
-function Criminal(name, gender, age, bio, image) {
+function Criminal(name, age, bio, image, num) {
   this.name = name;
-  this.gender = gender;
   this.age = age;
   this.bio = bio;
   this.image = image;
   this.num = 0;
-  this.adder = function() {
-    this.num++;
-  };
-  this.subtract = function() {
-    this.num--;
-  };
 }
 
-var crim1 = new Criminal("bob", "male", 15);
+var crim1 = new Criminal(
+  "Angela",
+  "99",
+  "Lorem Ipsum",
+  "http://placekitten.com/500/500",
+  0
+);
 
-var crim2 = new Criminal("joe", "male", 10);
+var crim2 = new Criminal(
+  "Bob",
+  "12",
+  "Lorem Ipsum",
+  "http://placekitten.com/500/500",
+  1
+);
 
 function test() {
   document.querySelector("h1").innerHTML = crim1.name;
   crim1.printName;
-}
-
-function increment() {
-  crim1.adder();
-}
-
-function decrement() {
-  crim1.subtract();
 }
