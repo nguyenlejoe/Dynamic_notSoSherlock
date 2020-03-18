4a70feea554f83aa694b8060275fa06e0bfd3465
Noah Mah <noahmah@noahs-mbp.wifi.bcit.ca>
2020-03-17 19:00:54 -0700

    Combined final index, don't work well, JOE HELP

--- Joe_ObjectGen/objectGen.js
+++ Joe_ObjectGen/objectGen.js
@@ -3,37 +3,39 @@ var mainCrime;
 var mainHouse;
 var firstNight = true;
 
+function gameGen() {
+  if (firstNight === true) {
+    //Random Generation of number between 0-2 for each object
+    var culpritNum = Math.floor(Math.random() * dayState.culpritTrack.length);
+    var crimeNum = Math.floor(Math.random() * dayState.crimeTrack.length);
+    var houseNum = Math.floor(Math.random() * dayState.houseTrack.length);
 
-function gameGen(){
+    //Grabbing properties from object depending on the number
+    mainCulprit = dayState.culpritTrack[culpritNum];
+    mainCrime = dayState.crimeTrack[crimeNum];
+    mainHouse = dayState.houseTrack[houseNum];
 
-    if(firstNight === true){
-        //Random Generation of number between 0-2 for each object
-        var culpritNum = Math.floor(Math.random() * dayState.culpritTrack.length);
-        var crimeNum =  Math.floor(Math.random() * dayState.crimeTrack.length);
-        var houseNum = Math.floor(Math.random() * dayState.houseTrack.length);
+    firstNight = false;
 
-        //Grabbing properties from object depending on the number
-        mainCulprit = dayState.culpritTrack[culpritNum];
-        mainCrime = dayState.crimeTrack[crimeNum];
-        mainHouse = dayState.houseTrack[houseNum];
+    console.log(
+      "culprit is " +
+        mainCulprit +
+        " and the crime is " +
+        mainCrime +
+        " is in " +
+        mainHouse +
+        " house"
+    );
+  } else {
+    crimeNum = Math.floor(Math.random() * dayState.crimeTrack.length);
+    houseNum = Math.floor(Math.random() * dayState.houseTrack.length);
 
-        firstNight = false;
+    mainCrime = dayState.crimeTrack[crimeNum];
+    mainHouse = dayState.houseTrack[houseNum];
 
-        console.log("culprit is " + mainCulprit + " and the crime is " + mainCrime + " is in " + mainHouse + " house" );
-    }
-    else{
-        crimeNum =  Math.floor(Math.random() * dayState.crimeTrack.length);
-        houseNum = Math.floor(Math.random() * dayState.houseTrack.length);
-
-        mainCrime = dayState.crimeTrack[crimeNum];
-        mainHouse = dayState.houseTrack[houseNum];
-
-        dayState.crimeTrack.splice(crimeNum,1);
-        dayState.houseTrack.splice(houseNum,1);
-
-    }
-    
+    dayState.crimeTrack.splice(crimeNum, 1);
+    dayState.houseTrack.splice(houseNum, 1);
+  }
 }
 
 gameGen();
-
