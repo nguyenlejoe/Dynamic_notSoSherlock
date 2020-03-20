81c7c5f25170e6da27d6c1d923cf4c88c38421a2
nguyenlejoe <joe_le.1@hotmail.com>
2020-03-20 14:08:02 -0700

    some changes

--- Joe_PageTransitions/transition.js
+++ Joe_PageTransitions/transition.js
@@ -7,6 +7,7 @@ function pageSlide(current, next){
     if(appOverFlow = false){
         document.querySelector("#" + next).style.display = "flex";
         document.querySelector("#" + current).style.display = "none";
+        document.querySelector("#app").style.overflow = "hidden";
         console.log(next);
     }
     else{
--- Noah - Carousel/carousel.js
+++ Noah - Carousel/carousel.js
@@ -180,8 +180,6 @@ function checkRecycle(){
   }
 }
 
-
-
 }
 
 
