d405238c5d3595e06015c740b977bebd318e905f
nguyenlejoe <joe_le.1@hotmail.com>
2020-03-20 23:01:39 -0700

    added houseCheck function to turn them grey

--- Joe_CrimeHouseCheck/crimeHouse.js
+++ Joe_CrimeHouseCheck/crimeHouse.js
@@ -1,3 +1,5 @@
+var checkedHouse = [];
+
 //Checks if user has picked the house that the crime has occured in
 
 function checkCrimeHouse(houseNum) {
@@ -7,59 +9,62 @@ function checkCrimeHouse(houseNum) {
         //Next page will involve scrolling so turn overflow on
         appOverFlow = true;
         //Lets users know crime has been found if correct
-        alert("The crime has been found!");
-        //Transition to "MultiChoice.js" or clueStage
         pageSlide("townPage", "clueStage");
+  
         //Adds choices to the page -- function found in "MultiChoice.js"
         addChoices();
+
+        displayHouseGray(1);
+
       } else {
         //If user picks a house that does not have a crime in it
-        alert("seems to be nothing here");
+        alert("Nothing");
       }
       break;
 
     case 2:
       if (mainHouse === "Alex") {
         appOverFlow = true;
-        alert("The crime has been found!");
         pageSlide("townPage", "clueStage");
         addChoices();
+        displayHouseGray(2);
       } else {
-        alert("seems to be nothing here");
+        alert("Nothing");
       }
       break;
 
     case 3:
       if (mainHouse === "Noah") {
         appOverFlow = true;
-        alert("The crime has been found!");
         pageSlide("townPage", "clueStage");
         addChoices();
+        displayHouseGray(3);
       } else {
-        alert("seems to be nothing here");
+        alert("Nothing");
       }
       break;
 
     case 4:
       if (mainHouse === "Joe") {
         appOverFlow = true;
-        alert("The crime has been found!");
         pageSlide("townPage", "clueStage");
         addChoices();
+        displayHouseGray(4);
       } else {
-        alert("seems to be nothing here");
+        alert("Nothing");
       }
       break;
 
     case 5:
       if (mainHouse === "Aksel") {
         appOverFlow = true;
-        alert("The crime has been found!");
         pageSlide("townPage", "clueStage");
         addChoices();
+        displayHouseGray(5);
       } else {
-        alert("seems to be nothing here");
+        alert("Nothing");
       }
       break;
   }
 }
+
